ce2489f7e082f7fb4ee939a5eff54313f9615b1b
manavo <manavo@gmail.com>
2014-10-23 18:41:39 +0100

    Get issues of a project

--- src/Project.php
+++ src/Project.php
@@ -36,6 +36,40 @@ class Project
     }
 
     /**
+     * Get all the issues associated with this project
+     *
+     * @return array
+     */
+    public function issues()
+    {
+        return $this->client->get('projects/' . $this->id . '/issues/all');
+    }
+
+    /**
+     * Get all the active issues associated with this project
+     *
+     * @return array
+     */
+    public function activeIssues()
+    {
+        return $this->client->get(
+            'projects/' . $this->id . '/issues/all_active'
+        );
+    }
+
+    /**
+     * Get all the closed and fixed issues associated with this project
+     *
+     * @return array
+     */
+    public function closedAndFixedIssues()
+    {
+        return $this->client->get(
+            'projects/' . $this->id . '/issues/all_closed_and_fixed'
+        );
+    }
+
+    /**
      * Add a new issue to the project
      *
      * @param Issue $issue
@@ -45,8 +79,7 @@ class Project
     public function addIssue($issue)
     {
         return $this->client->post(
-            'projects/' . $this->id . '/issues',
-            $issue->toArray()
+            'projects/' . $this->id . '/issues', $issue->toArray()
         );
     }
 

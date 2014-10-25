a5a98bc8c24b2374d4a21b2df7e53e00fc55ec85
manavo <manavo@gmail.com>
2014-10-25 12:46:52 +0100

    More unit tests

--- tests/IssueTest.php
+++ tests/IssueTest.php
@@ -8,6 +8,11 @@ use PHPUnit_Framework_TestCase;
 class IssueTest extends PHPUnit_Framework_TestCase
 {
 
+    public function setUp()
+    {
+        date_default_timezone_set('UTC');
+    }
+
     public function testRequiredParametersIncludedWhenConvertingToArray()
     {
         $issueArray = (new Issue())->toArray();
@@ -36,12 +41,57 @@ class IssueTest extends PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('user_ids_to_cc', $issue->toArray());
     }
 
+    public function testDueDateIsAddedIfNotEmpty()
+    {
+        $issue = new Issue();
+        $issue->setDueDate('2014-01-12 12:01:00');
+
+        $this->assertArrayHasKey('due_date', $issue->toArray());
+    }
+
+    public function testTagsIsAddedIfNotEmpty()
+    {
+        $issue = new Issue();
+        $issue->setTags('tag1,tag2');
+
+        $this->assertArrayHasKey('tags', $issue->toArray());
+    }
+
     public function testUserIdsToCcIsACommaSeparatedString()
     {
         $issue = new Issue();
         $issue->setUserIdsToCc([1, 2, 3]);
 
         $this->assertEquals('1,2,3', $issue->toArray()['user_ids_to_cc']);
+    }
+
+    public function testTagsIsACommaSeparatedString()
+    {
+        $issue = new Issue();
+        $issue->setTags(['tag1', 'tag2', 'tag3']);
+
+        $this->assertEquals('tag1,tag2,tag3', $issue->toArray()['tags']);
+    }
+
+    public function testAttachmentsGetAddedWhenConvertingToArray()
+    {
+        $issue = new Issue();
+        $issue->addAttachment(__FILE__);
+        $issue->addAttachment(__FILE__);
+        $issue->addAttachment(__FILE__);
+        $issue->addAttachment(__FILE__);
+
+        $this->assertEquals(8, count($issue->toArray()));
+    }
+
+    public function testUnixTimestampGetsConvertedForDueDate()
+    {
+        $time = time();
+
+        $issue = new Issue();
+        $issue->setDueDate($time);
+
+        $this->assertEquals(date('Y-m-d H:i:s', $time), $issue->toArray()['due_date']);
     }
 
 }

4a5a1c9632045e82f4b15aa2c1f9814b740dc455
Philip Manavopoulos <manavo@gmail.com>
2016-04-08 18:55:19 +0100

    Update IssueTest.php

--- tests/IssueTest.php
+++ tests/IssueTest.php
@@ -151,48 +151,4 @@ class IssueTest extends PHPUnit_Framework_TestCase
         $client->project(123)->issue(111)->$function();
     }
 
-    public function typeOfRequestWithArgumentProvider()
-    {
-        return [
-            ['addComment', 'post', new Comment()],
-            ['updateStatus', 'put', 1],
-            ['updatePriorityLevel', 'put', 1],
-            ['updateTester', 'put', 1],
-            ['updateFixer', 'put', 1],
-        ];
-    }
-
-    /**
-     * @dataProvider typeOfRequestWithArgumentProvider
-     */
-    public function testMethodsWithArgumentMakeCorrectTypeOfRequest(
-        $function,
-        $requestType,
-        $argument
-    ) {
-        $guzzleClientMock = $this->getMockBuilder('\GuzzleHttp\Client')
-            ->disableOriginalConstructor()->getMock();
-        $guzzleClientMock->expects($this->once())->method($requestType);
-
-        $client = new Client('team', 'username', 'password');
-        $client->setClient($guzzleClientMock);
-
-        $client->project(123)->issue(182)->$function($argument);
-    }
-
-    public function testUpdateMethodSendsCommentIfSet()
-    {
-        $guzzleClientMock = $this->getMockBuilder('\GuzzleHttp\Client')
-            ->disableOriginalConstructor()->getMock();
-        $guzzleClientMock->expects($this->once())->method('put')
-            ->with($this->equalTo('https://team.mydonedone.com/issuetracker/api/v2/projects/111/issues/321/fixer.json'), $this->equalTo(['body' => ['new_fixer_id' => 1, 'comment' => 'Comment!']]));
-
-        $client = new Client('team', 'username', 'password');
-        $client->setClient($guzzleClientMock);
-
-        $client->project(111)->issue(321)->updateFixer(
-            1, 'Comment!'
-        );
-    }
-
 }
